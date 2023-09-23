181a2af83b038b7718d913c645ca0fce539609a3
marvinbra <marvinbopp37@gmail.com>
2023-09-23 14:45:09 +0200

    BlogSpace code
    layout and api placeholder

---
+++ index.js
@@ -0,0 +1,12 @@
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        for (let post of postsArr) {
+            document.getElementById("blog-list").innerHTML += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+    })
