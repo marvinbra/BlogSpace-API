05500a798f37e2e059a09804eda179f07781fb2f
marvinbra <marvinbopp37@gmail.com>
2023-09-26 19:25:06 +0200

    render and send data from api

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const postBtn = document.getElementById("post-btn")
+const blogList = document.getElementById("blog-list")
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
         const postsArr = data.slice(0, 5)
         for (let post of postsArr) {
-            document.getElementById("blog-list").innerHTML += `
+            blogList += `
                 <h3>${post.title}</h3>
                 <p>${post.body}</p>
                 <hr />
@@ -15,7 +16,28 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
 
 postBtn.addEventListener("click", (e) => {
     e.preventDefault()
-    const title = document.getElementById("post-title").value
-    const body = document.getElementById("post-body").value
-    console.log({title: title, body: body})
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+        method: "POST",
+        body: JSON.stringify({
+            data
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+                blogList.innerHTML += `
+                    <h3>${data.title}</h3>
+                    <p>${data.body}</p>
+                    <hr />
+                `
+        })
 })
